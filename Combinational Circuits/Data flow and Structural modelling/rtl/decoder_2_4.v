9f23a18e3c6d5d6de38fa2a6745f7bac52efd28f
Yash Pamnani <33838632+YashPamnani@users.noreply.github.com>
2021-04-07 02:33:32 +0530

    Update decoder_2_4.v

--- Combinational Circuits/Data flow and Structural modelling/rtl/decoder_2_4.v
+++ Combinational Circuits/Data flow and Structural modelling/rtl/decoder_2_4.v
@@ -1,1 +1,19 @@
+/********************************************************************************************
+Filename    :	   decoder_2_4.v   
 
+Description :      RTL for 2:4 decoder using data flow abstraction
+
+Author Name :      Yash Pamnani
+
+*********************************************************************************************/
+
+module decoder_2_4 (I, Y);
+	input [1:0] I;
+	output [3:0] Y;
+	
+	assign Y[0] = ~I[1] & ~I[0];
+	assign Y[1] = ~I[1] & I[0];
+	assign Y[2] = I[1] & ~I[0];
+	assign Y[3] = I[1] & I[0];
+	
+endmodule
